ef3124b2798ad408d5a96ecfe0b8cdf8af5444f5
Ido Calman <ido@storemaven.com>
2017-10-14 19:59:13 +0300

    MongoDB connection through /stream route

--- routes/stream.js
+++ routes/stream.js
@@ -1,23 +1,46 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import Promise from 'bluebird';
 
 const router = express.Router();
 
+mongoose.Promise = Promise;
 
 /* Stream object URI */
 router.post('/', (req, res) => {
+
     // Produce mongoDB URI from request
     let address = req.body.address;
     let port = req.body.port;
     let username = req.body.username;
     let password = req.body.password;
-    let uri = 'mongodb://' + username + ':' + password + '@' + address + ':' + port;
-
-    console.log(uri);
+    let database = 'streamableDb';
+    let uri = 'mongodb://' + username + ':' + password + '@' + address + ':' + port + '/' + database;
 
     // Connect to mongoDB
-    // mongoose.connect(uri);
+    let promise = mongoose.connect(uri, {
+        useMongoClient: true,
+        promiseLibrary: Promise
+    });
 
-    let database = 'streamableDb';
+    promise.then( (db, err) => {
+        // Users collection querying
+        let schema = new mongoose.Schema({
+            id: Number,
+            first_name: String,
+            last_name: String,
+            email: String,
+            gender: String,
+            ip_address: String
+        });
+
+        let User = mongoose.model('User', schema);
+
+        User.findOne({first_name : 'Henri'}, (err, user) => {
+            console.dir(user);
+        });
+
+    });
 
 });
 
