b3a886265662cfbaf74bf0a5c24775fd8da045d5
Ido Calman <ido@storemaven.com>
2017-10-14 03:03:33 +0300

    Bootstrap and form

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
 /* GET index page. */
 router.get('/', (req, res) => {
-  res.json({
-    title: 'Express'
-  });
+    // res.json({
+    //     title: 'Express'
+    // });
+    res.render('index');
+});
+
+/* Stream object URL */
+router.get('/stream', (req, res) => {
+    res.json({
+        table: 'table1',
+        data: {
+            key: 'val',
+            keyIndex: 2,
+            prop: 'Hello'
+        }
+    });
 });
 
 export default router;
